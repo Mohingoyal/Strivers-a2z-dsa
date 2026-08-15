d3f95efb4def5f7fd6ed7294f361db496d51a1af
Mohingoyal <mgoyal4_be25@thapar.edu>
2026-08-15 16:26:10 +0530

    Time: 248 ms (6.37%), Space: 600 MB (7.82%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<char> s1;
+        vector<char> s2;
+        
+         for(int i=0;i<s.length();i++)
+         {if(find(s1.begin(),s1.end(),s[i])==s1.end())//hai nhi
+         { 
+         s1.push_back(s[i]);
+            
+         }
+       
+        if(find(s2.begin(),s2.end(),t[i])==s2.end())//hai nhi
+         { 
+         s2.push_back(t[i]);
+         }
+        
+         }
+         string f="";
+if(s1.size()!=s2.size())
+return false;
+map<char,char> mp;
+for(int j=0;j<s1.size();j++)
+{
+    mp[s1[j]]=s2[j];
+} 
+for(auto k:s)
+{
+    f=f+mp[k];
+}
+if(f==t)
+return true;
+return false;
+    }
+};
