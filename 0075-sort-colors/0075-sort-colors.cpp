f102700c37a35e56631efb9cfd3476fe34b2cdb6
Mohingoyal <mgoyal4_be25@thapar.edu>
2026-08-18 16:50:42 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,15 +3,14 @@ public:
     void sortColors(vector<int>& nums) {
      int count0=0;
      int count1=0;
-     int count2=0;
+    
      for(auto k:nums)
      {
         if(k==0)
         count0++;
          if(k==1)
         count1++;
-         if(k==2)
-        count2++;
+       
      }int n=0;
      int size=nums.size();
      for(int i=0;i<size;i++)
@@ -27,11 +26,11 @@ if(count0==0&&count1!=0)
         n++;
         count1--;
      }
-     if(count0==0&&count1==0&&count2!=0)
+     if(count0==0&&count1==0&&n<nums.size())
      {
         nums[n]=2;
         n++;
-        count2--;
+        
      }
      }
     }
